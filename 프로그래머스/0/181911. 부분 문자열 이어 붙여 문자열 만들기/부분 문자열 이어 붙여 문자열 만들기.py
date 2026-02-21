ffdf5fc478a89169927800aca31a66c279b0e801
yejunedev <yejune.j12@gmail.com>
2026-02-21 12:39:40 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.03 ms, Memory: 9.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.py
@@ -0,0 +1,7 @@
+def solution(my_strings, parts):
+    answer = ''
+    for i in range(len(my_strings)):
+        s = parts[i][0]
+        e = parts[i][1]
+        answer+=my_strings[i][s:e+1]
+    return answer
