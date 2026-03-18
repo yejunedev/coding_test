2861e48938bebb470bf5417539a62c85e1e792c5
yejunedev <yejune.j12@gmail.com>
2026-03-18 09:58:38 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.00 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -0,0 +1,8 @@
+def solution(myString, pat):
+    answer = 0
+    myString = myString.replace('A','C').replace('B','A').replace('C','B')
+    if pat in myString:
+        return 1
+    else:
+        return 0
+    return answer
