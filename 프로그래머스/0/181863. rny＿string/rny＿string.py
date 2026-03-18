264dcf527fd8b4f26f6fb5ba98284a5885df6034
yejunedev <yejune.j12@gmail.com>
2026-03-18 09:44:40 +0900

    [level 0] Title: rny_string, Time: 0.02 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.py
@@ -0,0 +1,10 @@
+def solution(rny_string):
+    answer = ''
+    for i in rny_string:
+        if i=='m':
+            answer+='rn'
+        else:
+            answer+=i
+    return answer
+
+#   return rny_string.replace('m',rn)
